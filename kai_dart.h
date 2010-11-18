464fc97fe46053b472619a649690ad090ce0ba91
KO Myung-Hun <komh@chollian.net>
2010-11-18 16:07:23 +0000

    Remove logs

--- kai_dart.h
+++ kai_dart.h
@@ -17,45 +17,6 @@
     You should have received a copy of the GNU Library General Public
     License along with this library; if not, write to the Free
     Software Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-
-    Changes :
-        KO Myung-Hun <komh@chollian.net> 2007/02/11
-            - Removed the following variables in DARTSTRUCT
-                BOOL  fResampleFlg;
-                BOOL  fNormalizeFlg;
-                ULONG ulLastTimeStamp;
-                ULONG ulCurrTimeStamp;
-
-            - Removed the following macros
-                DART_CH_ALL      0
-                DART_CH_LEFT     1
-                DART_CH_RIGHT    2
-
-            - Instead, use MCI_SET_AUDIO_* macros.
-
-        KO Myung-Hun <komh@chollian.net> 2007/02/16
-            - Changed fStopped to fPlaying in DARTSTRUCT
-
-        KO Myung-Hun <komh@chollian.net> 2007/02/25
-            - Removed the following variables in DARTSTRUCT
-                BOOL               fPaused
-                BOOL               fWaitStreamEnd
-                ULONG              ulBytesPlayed
-                ULONG              ulSeekPosition
-                BOOL               fShareable
-                ULONG              ulCurrVolume
-                BOOL               fSamplesPlayed
-                USHORT             usDeviceID
-                PMCI_MIX_BUFFER    pMixBuffers
-                MCI_MIXSETUP_PARMS MixSetupParms
-                MCI_BUFFER_PARMS   BufferParms
-                PFNDICB            pfndicb
-
-        KO Myung-Hun <komh@chollian.net> 2008/03/30
-            - Added callback data as a parameter to dartInit()
-
-        KO Myung-Hun <komh@chollian.net> 2010/01/09
-            - Revised for KAI
 */
 
 #ifndef __KAI_DART_H__
