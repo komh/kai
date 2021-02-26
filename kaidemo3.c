4a0ac49fe00bbbcaa92ed45d6382ae8e40e1bdca
KO Myung-Hun <komh@chollian.net>
2021-02-26 22:29:44 +0900

    Add missing licenses
        modified:   kai_audiobuffer.c
        modified:   kai_audiobuffer.h
        modified:   kaidemo.c
        modified:   kaidemo2.c
        modified:   kaidemo3.c

--- kaidemo3.c
+++ kaidemo3.c
@@ -1,3 +1,16 @@
+/*
+ * KAI DEMO3 for KAI Mixer
+ * Copyright (C) 2010-2021 KO Myung-Hun <komh@chollian.net>
+ *
+ * This file is a part of K Audio Interface.
+ *
+ * This program is free software. It comes without any warranty, to
+ * the extent permitted by applicable law. You can redistribute it
+ * and/or modify it under the terms of the Do What The Fuck You Want
+ * To Public License, Version 2, as published by Sam Hocevar. See
+ * http://www.wtfpl.net/ for more details.
+ */
+
 #define INCL_KBD
 #define INCL_DOS
 #include <os2.h>
